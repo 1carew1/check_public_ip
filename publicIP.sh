ccdb39939f98816c2c125b6f82c8e658a2bcbc9a
Colm Carew <colmcarew2@gmail.com>
2017-02-10 09:40:04 +0000

    Adding Null Check

--- publicIP.sh
+++ publicIP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 0,30 * * * * /home/pi/script/publicIP.sh > /home/pi/script/checkPublicIp.log 2>&1
+# */15 * * * * /home/pi/script/publicIP.sh > /home/pi/script/checkPublicIp.log 2>&1
 DIRECTORY='/home/pi/script'
 OUTPUT_FILE="$DIRECTORY/whats_my_ip.txt"
 CURRENT_IP_FILE="$DIRECTORY/current_ip_address.txt"
@@ -11,7 +11,7 @@ DATE=$(date)
 
 echo "`date` - Adding Public IP to $OUTPUT_FILE"
 echo "$DATE - $IP_ADDRESS" >> $OUTPUT_FILE
-  if [ "$CURRENT_IP" != "$IP_ADDRESS" ]; then
+  if [ "$CURRENT_IP" != "$IP_ADDRESS" ] && ["$IP_ADDRESS" != ""] ; then
            echo "`date` - IP address has changed from $CURRENT_IP to $IP_ADDRESS"
        echo "IP Address has changed to : $IP_ADDRESS" | mail -s "Your public home ip is now : $IP_ADDRESS" - $EMAIL_RECEIPIENT
        echo $IP_ADDRESS > $CURRENT_IP_FILE
