046ce846e7d7f1a40d5274761c25aa69d2a70902
Colm Carew <colmcarew2@gmail.com>
2017-02-10 09:48:33 +0000

    Changing how public ip is obtained and checking for blanks

--- publicIP.sh
+++ publicIP.sh
@@ -6,13 +6,14 @@ OUTPUT_FILE="$DIRECTORY/whats_my_ip.txt"
 CURRENT_IP_FILE="$DIRECTORY/current_ip_address.txt"
 EMAIL_RECEIPIENT='colmcarew2@gmail.com'
 CURRENT_IP=$(cat $CURRENT_IP_FILE)
-IP_ADDRESS=$(wget http://ipecho.net/plain -O - -q ; echo)
+#IP_ADDRESS=$(wget http://ipecho.net/plain -O - -q ; echo)
+IP_ADDRESS=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
 DATE=$(date)
 
 echo "`date` - Adding Public IP to $OUTPUT_FILE"
 echo "$DATE - $IP_ADDRESS" >> $OUTPUT_FILE
-  if [ "$CURRENT_IP" != "$IP_ADDRESS" ] && ["$IP_ADDRESS" != ""] ; then
-           echo "`date` - IP address has changed from $CURRENT_IP to $IP_ADDRESS"
+  if [ "$CURRENT_IP" != "$IP_ADDRESS" ] && [ "" !=  "$IP_ADDRESS" ] ; then
+       echo "`date` - IP address has changed from $CURRENT_IP to $IP_ADDRESS"
        echo "IP Address has changed to : $IP_ADDRESS" | mail -s "Your public home ip is now : $IP_ADDRESS" - $EMAIL_RECEIPIENT
        echo $IP_ADDRESS > $CURRENT_IP_FILE
   else
